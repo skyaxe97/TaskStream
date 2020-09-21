d1a02f64eec27fe99f4fb0b692145ae181661e2b
skyaxe97 <skyaxe97@gmail.com>
2020-09-21 12:58:30 +0800

    Add countDeadlines and printData using streams methods

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -34,10 +34,23 @@ public class Main {
         return count;
     }
 
+    public static int countDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        int count;
+        count = (int) tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataUsingStreams (ArrayList<Task> tasksData) {
+        tasksData.stream()
+                .forEach(System.out::println);
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
